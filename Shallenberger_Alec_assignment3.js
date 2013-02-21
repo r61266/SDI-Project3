80cba8b6cd742f6b2c32e6c885608cf0d5d183a5
Alec Shallenberger <Alec@Alecs-MacBook-Pro.local>
2013-02-21 02:41:57 -0500

    Adding information to Javascript.

--- Shallenberger_Alec_assignment3.js
+++ Shallenberger_Alec_assignment3.js
@@ -1,1 +1,26 @@
 alert("JavaScript works!");
+
+// Alec Shallenberger
+// Febuary 21, 2013
+// Term 1302
+// SDI-Project 3
+
+// Variables
+var Restraunts = [
+	"Olive Garden",
+	"Red Lobster",
+	"Stevie B's",
+	"Outback Steakhouse"
+]
+console.log( Restraunts )
+var EstimatedPrice = [
+	25,
+	30,
+	15,
+	20
+]
+console.log( EstimatedPrice )
+
+//Output
+console.log("My choices four choices of restraunts are " + Restraunts + " ,but I don't know which one I want to go to.")
+console.log("The prices for each restraunt in order are " + EstimatedPrice + ".")
