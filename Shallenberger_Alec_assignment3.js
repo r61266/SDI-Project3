3a007deea6899048286d830104aaf1b794ac8ccf
Alec Shallenberger <Alec@Alecs-MacBook-Pro.local>
2013-02-21 04:32:27 -0500

    Adding Numbers with Loop in Javascript.

--- Shallenberger_Alec_assignment3.js
+++ Shallenberger_Alec_assignment3.js
@@ -5,7 +5,7 @@ alert("JavaScript works!");
 // Term 1302
 // SDI-Project 3
 
-// Variables
+// Array
 var Restraunts = [
 	"Olive Garden",
 	"Red Lobster",
@@ -20,7 +20,23 @@ var EstimatedPrice = [
 	20
 ]
 console.log( EstimatedPrice )
-
-//Output
-console.log("My choices four choices of restraunts are " + Restraunts + " ,but I don't know which one I want to go to.")
+console.log("My four choices of restraunts are " + Restraunts + " ,but I don't know which one I want to go to.")
 console.log("The prices for each restraunt in order are " + EstimatedPrice + ".")
+
+// Boolean, Return
+var Restraunt = function(name) {
+	var EstimatedPrice = (25, 30, 15, 20), 
+		Place, Places
+	if (EstimatedPrice < 40) { Place = "will try a different restraunt"; }
+	else (EstimatedPrice > 40) ;{ Places = "will definitely be eating here tonight!"; }
+	Place = "I " + Place + ", if it is to expensive."
+	Places = "I " + Places + ", if it is just the right price."
+	return Places;
+}; 
+var PlaceToEat = Restraunt("Olive Garden", "Red Lobster", "Stevie B's", "Outback Steakhouse")
+console.log(PlaceToEat);
+
+// 
+
+
+
